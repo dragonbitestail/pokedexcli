88567bc2944cdc02ba51bb55c788c807beaf9aa6
dragonbitestail <dragonbitestail@users.noreply.github.com>
2026-08-13 14:07:07 -0400

    Add file in main package to handle http req/resp

---
+++ utilHTTP.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+	"strings"
+)
+
+type requestType string
+
+const (
+    reqGetC requestType  = "GET"
+    reqPostC requestType = "POST"
+)
+   
+func getResponse(uri string, rType requestType) (*http.Response, error) {
+    // Create new request:
+    req, err := http.NewRequest(string(rType), uri, nil)
+		// OR, to include data in the request body as a chunk of bytes. In this case jsonData
+		// would usually be a some json.Marshel'd struct.
+    //req, err := http.NewRequest(string(rType), uri, bytes.NewBuffer(jsonData))
+    if err != nil {
+      fmt.Println("getLastModified(): Returning error for creating request.")
+      return nil, err
+    }
+    
+    // Set header on the request:
+    // req.Header.Set("x-api-key", "123456789")
+    
+    // Make the request using '&'.
+		// Ensures you won't accidentally duplicate the client state when passing it around your code.
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+      fmt.Println("getLastModified(): Returning error for making request.")
+      return nil, err
+    }
+    // CALLER IS EXPECTED TO CLOSE THE BODY!!!! defer resp.Body.Close()
+		// OR, optionally, change func behavior to return contents of the body and not the response object.
+
+
+		//printAllRespHeaders(resp.Header)
+		logr.Info(getAllRespHeaders(resp.Header))
+
+    return resp, nil
+}
+  
+
+func printAllRespHeaders(respHeader http.Header){
+    fmt.Println("RESPONSE Headers::")
+    for field, value := range respHeader {
+        fmt.Printf("%s => %s\n", field, value)
+    }
+	return 
+}
+
+func getAllRespHeaders(respHeader http.Header) string {
+		var lines []string
+		lines = append(lines, "HTTP Reponse HEADERS")
+    for field, value := range respHeader {
+			line := fmt.Sprintf("%s: %s", field, value)
+			lines = append(lines, line)
+    }
+	return strings.Join(lines, "|")
+}
